bd6b2f9808e82fa8b61f63f0cb1b5ed2e70455e0
JesseB) <jeslbarr@iu.edu>
2021-09-20 16:09:22 -0400

    began scaffolding the javascript

---
+++ labs/lab04/app.js
@@ -0,0 +1,4 @@
+//TODO ANIMATIONS
+//1: build in for all items on the screen (nothing to something)
+//2: a mouseOver/highlight for each of the gallery elements
+//3: an exit animation for when a gallery element is clicked
