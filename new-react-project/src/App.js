31a9e544f19a0cfc7509c7f821183085c4858ab5
JesseB) <jeslbarr@iu.edu>
2021-10-19 13:45:23 -0400

    tried a react project in vs code and created my connect 4 game

---
+++ new-react-project/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          Watch it spin!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Much fast!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
